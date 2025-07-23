53a9d043c0d9c2bf7e0046ee7579d9d7154b303d
Kazuyuki2025 <pbww1g8g@s.okayama-u.ac.jp>
2025-07-23 09:56:40 +0900

    update

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,38 @@
 class VideosController < ApplicationController
   def new
+    @video = Video.new
   end
 
+  def create
+    @video = Video.new(video_params)
+
+    if params[:video][:file].blank?
+      flash[:alart] = "ファイルを選択してください"
+      render :new and return
+    end
+
+    @video.file.attach(io: params[:video][:path].open, filename: params[:video][:path].original_filename)
+
+    video_path = ActiveStorage::Blob.service.path_for(@video.file.key)
+
+    # コーデックの判定，"H.264"出でない場合は変換
+    movie = FFMPEG::Movie.new(video_path)
+    puts "---------------\nMovie Codec: #{movie.video_codec}\n"
+    if movie.video_codec != "h264"
+      h264_path = video_path.to_s.sub(/\.mp4\z/, "_h264.mp4")
+      movie.transcode(h264_path, %w[-vcodec libx264 -acodec aac -movflags +faststart])
+      FileUtils.mv(h264_path, video_path) # 上書き
+    end
+
+    # データベースに保存
+    if @video.save
+      flash[:notice] = "動画がアップロードされました"
+      redirect_to @video
+    else
+      flash[:alert] = "動画のアップロードに失敗しました"
+      render :new
+    end
+  end
   def index
   end
 
